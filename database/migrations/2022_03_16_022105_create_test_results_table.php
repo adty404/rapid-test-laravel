e3e9cf3c0ea45a6b5c8d9e6c5c8b023e22f7eb57
Aditya Prasetyo <adty404@gmail.com>
2022-03-16 22:58:44 +0700

    test result model -mfc

---
+++ database/migrations/2022_03_16_022105_create_test_results_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test_results', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_register_id')->constrained();
+            $table->string('result');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('test_results');
+    }
+}
